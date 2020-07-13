f304bd0b850ca2e80aa279828d01b5afac2c1378
Jon <orejajonathan@gmail.com>
2020-07-13 13:12:29 +0200

    food & snake

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Component } from 'react';
+import Snake from './Snake'
+import Food from './Food'
+
+const getRandomCoordinates = () => {
+  let min = 1
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2
+  return [x, y]
 
 
-function App() {
-  return (
-    <div className="game-area"></div>
+}
+
+
+class App extends Component{ 
+
+  state = {
+    food : getRandomCoordinates(),
+    snakeDots : [
+      [0, 0],
+      [2,0]
+    ]
+  }
+  render(){ 
+    return( 
+    <div className="game-area">
+      <Snake snakeDots={this.state.snakeDots}/>
+      <Food dot={this.state.food}/>
+
+
+    </div>
   );
+}
 }
 
 export default App;
