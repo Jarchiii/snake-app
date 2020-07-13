52aa4449fe977e7b77caa626e3b0748a46ad5531
Jon <orejajonathan@gmail.com>
2020-07-13 13:33:06 +0200

    snake move

--- src/App.js
+++ src/App.js
@@ -18,11 +18,66 @@ class App extends Component{
 
   state = {
     food : getRandomCoordinates(),
+    speed: 200,
+    direction : "RIGHT",
     snakeDots : [
       [0, 0],
       [2,0]
     ]
   }
+componentDidMount(){
+  setInterval(this.moveSnake, this.state.speed);
+  document.onkeydown = this.onKeyDown
+}
+
+onKeyDown = (e) => {
+  e = e  || window.event;
+  switch (e.keyCode) {
+    case 38:
+      this.setState({direction: 'UP'})
+      break;
+    case 40:
+      this.setState({direction: 'DOWN'})
+      break;
+    case 37:
+      this.setState({direction: 'LEFT'})
+      break;
+    case 39 : 
+    this.setState({direction : 'RIGHT'})
+      break;
+  }
+ 
+}
+
+moveSnake = () => {
+  let dots = [...this.state.snakeDots]
+  let head = dots[dots.length-1]
+
+  switch(this.state.direction){
+    case 'RIGHT' :
+      head = [head[0]+ 2, head [1]]
+    break;
+    case 'LEFT' : 
+    head = [head[0]- 2, head [1]]
+    break;
+    case 'DOWN' : 
+    head = [head[0], head [1]+2]
+    break;
+    case 'UP' : 
+    head = [head[0], head [1]- 2]
+    break;
+
+  }
+  dots.push(head)
+  dots.shift()
+  this.setState({
+    snakeDots : dots
+  })
+
+}
+
+
+
   render(){ 
     return( 
     <div className="game-area">
