1270fd7e9ab290c8cda8790db8a028db7e48c2a2
Jon <orejajonathan@gmail.com>
2020-07-13 17:53:17 +0200

    Updates

---
+++ precache-manifest.06ba7af787b57574288c5fd8bdd2ceb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af80ba18624b06c8ddb282d9be57c00a",
+    "url": "/snake-app/index.html"
+  },
+  {
+    "revision": "574781dcb0e948888657",
+    "url": "/snake-app/static/css/main.9d8fcac9.chunk.css"
+  },
+  {
+    "revision": "d1ccffe66ff33261462e",
+    "url": "/snake-app/static/js/2.ecb7a5a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-app/static/js/2.ecb7a5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574781dcb0e948888657",
+    "url": "/snake-app/static/js/main.26e1f5d2.chunk.js"
+  },
+  {
+    "revision": "499922a4f7ec5ad0ff9b",
+    "url": "/snake-app/static/js/runtime-main.acc96231.js"
+  }
+]);
